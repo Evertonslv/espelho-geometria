3454cfae2e095a47a9e61e014fd2220bde5316a7
evertonslv <evertonslv.silva@gmail.com>
2019-05-31 01:16:44 -0300

    Correcoes imagens

--- Assets/Scripts/Inical.cs
+++ Assets/Scripts/Inical.cs
@@ -6,19 +6,18 @@ using UnityEngine.SceneManagement;
 public class Inical : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { }
 
     public void iniciarBtn()
     {
         SceneManager.LoadScene("Tela2");
+    }
+
+    public void analizarImagemBtn()
+    {
+        SceneManager.LoadScene("Tela Accept");
     }
 }
